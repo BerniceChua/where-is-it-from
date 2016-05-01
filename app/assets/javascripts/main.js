cee7d892325756f9df6d702255d48078fdb43f1c
BerniceChua <Bernice.Chua.415@gmail.com>
2016-04-30 17:11:46 -0700

    Use jQuery to upload/scan image.

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('#scanImage').fileupload({
+        dataType: 'json',
+        add: function (e, data) {
+            data.context = $('<button/>').text('Upload')
+                .appendTo(document.body)
+                .click(function () {
+                    data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+                    data.submit();
+                });
+        },
+        done: function (e, data) {
+            data.context.text('Upload finished.');
+        }
+    });
+});
